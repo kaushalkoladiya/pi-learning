4210fad6cea8fb3786a4ac9b341e0fffa8531a7f
Kaushal <kaushalkoladiya123@gmail.com>
2024-06-03 17:29:51 -0400

    chore: remove code

--- server/routers/uploadRouter.js
+++ server/routers/uploadRouter.js
@@ -6,33 +6,6 @@ dotenv.config();
 
 const uploadRouter = express.Router();
 
-
-const resolveBlobName = (req, file) => {
-  return new Promise((resolve, reject) => {
-    const blobName = yourCustomLogic(req, file);
-    resolve(blobName);
-  });
-};
-
-const resolveMetadata = (req, file) => {
-  return new Promise((resolve, reject) => {
-    const metadata = yourCustomLogic(req, file);
-    resolve(metadata);
-  });
-};
-
-const resolveContentSettings = (req, file) => {
-  return new Promise((resolve, reject) => {
-    const contentSettings = yourCustomLogic(req, file);
-    resolve(contentSettings);
-  });
-};
-
-console.log(process.env.AZURE_STORAGE_CONNECTION_STRING);
-console.log(process.env.AZURE_STORAGE_CONTAINER_NAME);
-console.log(process.env.AZURE_STORAGE_ACCOUNT_NAME);
-console.log(process.env.AZURE_STORAGE_ACCESS_KEY);
-
 const azureStorage = new MulterAzureStorage({
   connectionString: process.env.AZURE_STORAGE_CONNECTION_STRING,
   accessKey: process.env.AZURE_STORAGE_ACCESS_KEY,
