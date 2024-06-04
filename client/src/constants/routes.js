4a98bf605ab684604116ac0d2f60d2eeb4f4635a
Kaushal <kaushalkoladiya123@gmail.com>
2024-06-03 21:50:52 -0400

    feat: added redux toolkit and protected routes

---
+++ client/src/constants/routes.js
@@ -0,0 +1,11 @@
+export const ROUTES = {
+  HOME: '/',
+  ABOUT: '/about',
+  LOGIN: '/auth/login',
+  REGISTER: '/auth/register',
+  PROFILE: '/profile',
+  ADMIN: '/admin',
+  NOT_FOUND: '/404',
+  UNAUTHORIZED: '/401',
+  SERVER_ERROR: '/500',
+};
