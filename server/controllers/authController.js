8e92267ecdb6da294197526415fa7a24dc9f0406
Kaushal <kaushalkoladiya123@gmail.com>
2024-06-12 10:56:40 -0400

    fix: auth middleware

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -16,6 +16,7 @@ const register = async (req, res) => {
         });
         res.status(201).json({ message: 'User registered successfully', user });
     } catch (err) {
+        console.log(err)
         res.status(500).json({ message: err.message });
     }
 };
