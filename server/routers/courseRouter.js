f0049880d9ac8b9daca2ee3a0386815b5bde2808
SUKHMEET SINGH <sukhmeetsandhu300198@gmail.com>
2024-06-13 21:13:49 +0530

    crud- courses

--- server/routers/courseRouter.js
+++ server/routers/courseRouter.js
@@ -1,40 +1,17 @@
 import express from 'express';
-import Course from '../models/CourseModel.js'; // Update the path as per your structure
-
+import {
+    createCourse,
+    getAllCourses,
+    getCourseById,
+    updateCourse,
+    deleteCourse
+} from '../controllers/courseController.js'; 
 const courseRouter = express.Router();
 
-// CREATE a new course
-courseRouter.post('/courses', async (req, res) => {
-    try {
-        const course = await Course.create(req.body);
-        res.status(201).send(course);
-    } catch (error) {
-        res.status(400).send(error);
-    }
-});
-
-// Read all courses
-courseRouter.get('/courses', async (req, res) => {
-    try {
-        const courses = await Course.findAll();
-        res.json(courses);
-    } catch (error) {
-        res.status(500).send({ error: "Error fetching courses: " + error.message });
-    }
-});
-
-// READ course by ID
-courseRouter.get('/courses/:id', async (req, res) => {
-    try {
-        const course = await Course.findByPk(req.params.id);
-        if (!course) {
-            return res.status(404).send({ error: 'Course not found' });
-        }
-        res.send(course);
-    } catch (error) {
-        res.status(500).send(error);
-    }
-});
-
+courseRouter.post('/courses', createCourse);
+courseRouter.get('/courses', getAllCourses);
+courseRouter.get('/courses/:id', getCourseById);
+courseRouter.put('/courses/:id', updateCourse);
+courseRouter.delete('/courses/:id', deleteCourse);
 
 export default courseRouter;
