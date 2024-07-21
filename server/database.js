271f3534ca1fdbc5fad41e51c296ab4fc03d0a2d
Kaushal <kaushalkoladiya123@gmail.com>
2024-07-21 12:28:50 -0400

    chore: change connection pattern

--- server/database.js
+++ server/database.js
@@ -2,16 +2,16 @@ import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const sequelize = new Sequelize({
-  dialect: 'postgres',
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  logging: true,
+  dialect: 'postgres',
+  port: 5432,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
 });
 
 export default sequelize;
-
-// database.js postgres conn
-
